14fd6f69703693da523ab09ef2be256b71e1c04e
Roman <neirity@gmail.com>
2026-01-27 17:52:27 +0200

    fix 3, 5-6, 9 tasks

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -9,7 +9,7 @@ export const registerUserSchema = {
 
 export const loginUserSchema = {
   [Segments.BODY]: Joi.object({
-    email: Joi.string().required(),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
 };
