b529f1696183bdd93391d42d156d87ba0dfd1130
Roman <neirity@gmail.com>
2026-01-27 17:33:35 +0200

    hw done

--- src/models/note.js
+++ src/models/note.js
@@ -20,6 +20,10 @@ const noteSchema = new Schema(
       enum: TAGS,
       default: 'Todo',
     },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
   { timestamps: true },
 );
--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 import { errors } from 'celebrate';
 
 const app = express();
@@ -21,21 +23,13 @@ app.use(logger);
 app.use(express.json());
 // Дозволяє запити з будь-яких джерел
 app.use(cors());
+app.use(cookieParser());
 
-// Кореневий маршрут
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello, World!' });
-});
-
+app.use(authRoutes);
 app.use(notesRoutes);
 
-// Middleware 404 (після всіх маршрутів)
 app.use(notFoundHandler);
-
-// обробка помилок від celebrate (валідація)
 app.use(errors());
-
-// Middleware для обробки помилок (останнє)
 app.use(errorHandler);
 
 // підключення до MongoDB
